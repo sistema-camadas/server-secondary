cd2254cb092eb26b1857631d3d01940df7a39641
freitas-davi <freitas.davi@sempreceub.com>
2025-06-17 19:47:16 -0300

    feat(controllers): Rota criar usuário

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -9,7 +9,7 @@ user_bp = Blueprint('user_bp', __name__)
 @user_bp.route('/users', methods=['GET']) 
 def get_all_users():
     users = User.query.all()
-    return success(data=[user.to_dict() for user in users])
+    return success(data=[user.to_dict() for user in users]) # Mensagem de sucesso ao buscar todos os usuários
 
 ## Buscar usuário por ID ##
 @user_bp.route('/users/<int:user_id>', methods=['GET'])
@@ -17,5 +17,15 @@ def get_user(user_id):
     user = User.query.get(user_id)
     if not user:
         return not_found()
-    return success(data=user.to_dict())
+    return success(data=user.to_dict()) # Mensagem de sucesso ao buscar usuário
+
+## Criar novo usuário ##
+@user_bp.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    user = User(name = data['name'], email = data['email'])
+    db.session.add(user)
+    db.session.commit()
+    return created(data=user.to_dict()) # Mensagem de sucesso ao criar usuário
+
 
