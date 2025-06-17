fe8054523c5bf4d224788972ad3dfa3a2e076200
freitas-davi <freitas.davi@sempreceub.com>
2025-06-17 15:56:57 -0300

    Rebuild da arquitetura

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from app.controllers.user_controller import user_bp
-    app.register_bluepreint(user_bp)
-
-    return app
