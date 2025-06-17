a0a017e34793d5efe4fcfb56ba21400568ac9ebb
freitas-davi <freitas.davi@sempreceub.com>
2025-06-17 19:10:26 -0300

    feat(models): Criando modelo de usuário

---
+++ models/user_model.py
@@ -0,0 +1,21 @@
+from models.database import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.timezone.utc)
+    updated_at = db.Column(db.DateTime, default=datetime.timezone.utc, onupdate=datetime.timezone.utc)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }
+
