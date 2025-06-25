aeb519c3279767475d80a4712a253278f5831cb4
freitas-davi <freitas.davi@sempreceub.com>
2025-06-25 08:32:29 -0300

    feat(app): Atualizando config de porta e DB_URL

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ app.register_blueprint(user_bp)
 app.register_blueprint(health_bp)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    port=int(os.environ.get('PORT', 10000))
+    app.run(host="0.0.0.0", port=port)
